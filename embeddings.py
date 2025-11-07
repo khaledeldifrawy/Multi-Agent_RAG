d58f8ff9cffefc3795cae91d9355e16491d5aaef
khaled <ka706788@gmail.com>
2025-11-07 17:29:41 +0200

    add cleaner retriever class

--- embeddings.py
+++ embeddings.py
@@ -11,3 +11,19 @@ class STEmbeddingsWrapper:
 
     def embed_query(self, text: str) -> List[float]:
         return self.model.encode([text], show_progress_bar=False, convert_to_numpy=True).tolist()[0]
+    
+class CleanRetriever:
+    def __init__(self, base_retriever):
+        self.base_retriever = base_retriever
+
+    def get_relevant_documents(self, query: str):
+        docs = self.base_retriever.get_relevant_documents(query)
+        # فلتر بسيط يحذف None / non-str / empty
+        docs = [d for d in docs if d.page_content and isinstance(d.page_content, str) and d.page_content.strip()]
+        return docs
+
+    async def aget_relevant_documents(self, query: str):
+        docs = await self.base_retriever.aget_relevant_documents(query)
+        docs = [d for d in docs if d.page_content and isinstance(d.page_content, str) and d.page_content.strip()]
+        return docs
+
